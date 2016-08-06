c2ae6022795e7e974c94c6e17ec91a74787469dc
marvan_t <marvan_t@hotmail.com>
2016-08-06 17:37:51 +0200

    wrote loops for getting data of the approprate type.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,11 +1,25 @@
 puts "What is the hamster's name?"
 hamster_name = gets.chomp
+
 puts "What is the hamster's volume level (1-10)?"
-hamster_volume = gets.chomp
+input = gets.chomp.to_i
+until input > 0 && input < 11
+	puts "Please enter a value between 1 - 10"
+	input = gets.chomp.to_i
+end
+hamster_volume = input
+
 puts "What is hamster's fur color?"
 hamster_color = gets.chomp
+
 puts "Is the hamster good candidate for adoption (Y/N)?"
-candidate = gets.chomp
+input = gets.chomp
+until input == "Y" || input == "N" || input == "y" || input == "n"
+	puts "Please enter Y for yes or N for no"
+	input = gets.chomp
+end
+candidate = input
+
 puts "What is the hamster's estimated age?"
 input = gets.chomp
 if input.to_i <= 0 
