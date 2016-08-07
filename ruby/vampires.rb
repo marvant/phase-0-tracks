3162a4b298192f50975d4bea41755f446e0940b0
marvan_t <marvan_t@hotmail.com>
2016-08-07 13:39:09 +0200

    wrote codes to match conditions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,15 +1,39 @@
 puts "What is your name?"
-name = gets.chomp
+name = gets.chomp.upcase
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-year_born = gets.chomp
+year_born = gets.chomp.to_i
+#calculating age from year born
+cal_age = 2016 - year_born
 
 puts "Our compnay cafeteria serves garlic bread.  Should we order some for you?"
-wants_garlic = gets.chomp
+wants_garlic = gets.chomp.upcase
 
 puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+insurance = gets.chomp.upcase
+
+if cal_age == age && (wants_garlic == "YES" || insurance == "YES")
+	condition = "Probably not a vampire."
+end
+
+if cal_age != age && (wants_garlic == "NO" || insurance == "NO")
+	condition = "Probably a vampire."
+end
+
+if cal_age != age && (wants_garlic == "NO" && insurance == "NO")
+	condition = "Almost certainly a vampire."
+end
+
+if name == "DRACULA" || name == "TU FANG" || name == "DRAKE CULA"
+	condition = "Definitely a vampire"
+end
+
+if condition == nil
+	condition = "Results Inconclusive"
+end
+puts condition
+
 
