ddf77cd62a645eb874497f880a6450ca18c28f34
marvan_t <marvan_t@hotmail.com>
2016-08-07 21:15:15 +0200

    make ajdustments for release 1 & 2

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,24 +1,31 @@
-puts "type is a word"
-index = 0
-str = gets.chomp
-
-while index < str.length
-	str[index] = str[index].next
-	index += 1
+def encrypt (str)
+	index = 0
+	while index < str.length
+		str[index] = str[index].next
+		index += 1
+	end
+	puts str
 end
-puts str
+
+puts "type in a word"
+str = gets.chomp
+encrypt (str)
+
+def decrypt (str)
+	i=0
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+	while i < str.length
+		x = alphabet.index(str[i])
+		x.to_i
+		y = x - 1
+		z = alphabet[y]
+		str [i] = z
+		i += 1
+	end
+	puts str
+end
 
 puts "type in another word"
-i=0
 str = gets.chomp
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-while i < str.length
-	x = alphabet.index(str[i])
-	x.to_i
-	y = x - 1
-	z = alphabet[y]
-	str [i] = z
-	i += 1
-end
-puts str
+decrypt(str)
