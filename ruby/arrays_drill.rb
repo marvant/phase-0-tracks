5d021b4ec8e592976ca5d5b79350550ed2b4fe01
marvan_t <marvan_t@hotmail.com>
2016-08-13 08:52:44 -0700

    complete everything in release 2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,11 @@
+def build_array(a, b, c)
+	new_array = [a, b, c]
+end
+
+def add_to_array(arr, item)	
+	arr << item
+end
+
 initial_array = []
 p initial_array
 
@@ -14,8 +22,11 @@ initial_array.shift
 p initial_array
 
 puts "this array contains item4: "
-p initial_array.include?("item9")
+p initial_array.include?("item4")
 
 second_array = [1,2,3]
 combined_array = initial_array + second_array
 p combined_array
+
+p build_array(4, 5, 6)
+p add_to_array(["a", "b", "c", 1, 2], 3)
