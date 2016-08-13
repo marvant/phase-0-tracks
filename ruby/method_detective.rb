1143e150d5ff6c91ff432a60b4b3bbdca1f8cd71
marvan_t <marvan_t@hotmail.com>
2016-08-13 09:08:30 -0700

    add methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,39 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1,'o')
 # => “zoom”
 
-# "enhance".<???>
+("enhance".ljust(11)).rjust(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".insert(9,' suspects')
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# each character is a represented by a number between 0 and 127. 
+# 122 is the assigned number for "z".  The opposite of ord is chr
+# 122.chr will return "z"
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
