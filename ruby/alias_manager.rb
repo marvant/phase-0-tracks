0a74abae992c6fc7742c9b314c218b82f656dc9b
marvan_t <marvan_t@hotmail.com>
2016-08-14 23:49:52 -0700

    release0 - Attemp a tricky algorithm

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,80 @@
+
+
+#git the first name and convert it to lower case letters
+puts "What is the first name?"
+first_name = gets.chomp.downcase
+
+#git the last name and convert it to the lower case letters
+puts "What is the last name?"
+last_name = gets.chomp.downcase
+
+
+#make an array for vowels 
+vowels = ["a", "e", "i", "o", "u"]
+
+#convert the first name to array
+first_name_array = first_name.split("")
+
+#change all the vowels to the next vowel in the alphabet and 
+#all the consonant to the next consonant in the alphabet
+#for the first name
+first_name_array.map! do |x|
+	if 
+		x == vowels[0]
+		x = vowels[1]
+	elsif 
+		x == vowels[1]
+		x = vowels[2]
+	elsif 
+		x == vowels[2]
+		x = vowels[3]
+	elsif 
+		x == vowels[3]
+		x = vowels[4]
+	elsif 
+		x == vowels[4]
+		x = vowels[0]
+	else
+		x = x.next
+	end
+end
+
+#convert first name array back to string and capitalize
+new_first_name = first_name_array.join("").capitalize!
+
+
+#convert the last name to array
+last_name_array = last_name.split("")
+
+#change all the vowels to the next vowel in the alphabet and 
+#all the consonant to the next consonant in the alphabet
+#for the last name
+last_name_array.map! do |x|
+	if 
+		x == vowels[0]
+		x = vowels[1]
+	elsif 
+		x == vowels[1]
+		x = vowels[2]
+	elsif 
+		x == vowels[2]
+		x = vowels[3]
+	elsif 
+		x == vowels[3]
+		x = vowels[4]
+	elsif 
+		x == vowels[4]
+		x = vowels[0]
+	else
+		x = x.next
+	end
+end
+
+#convert last name array back to string and capitalize
+new_last_name = last_name_array.join("").capitalize
+
+
+name = [new_first_name, new_last_name]
+name.reverse!
+result = name.join(" ")
+puts result
