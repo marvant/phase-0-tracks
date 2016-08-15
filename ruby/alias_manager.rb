0f90bf37425f3fad7376ce2710e4c93648e6aaa5
marvan_t <marvan_t@hotmail.com>
2016-08-15 00:00:52 -0700

    Release 1: provide a user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,9 +1,10 @@
 
 
 #git the first name and convert it to lower case letters
-puts "What is the first name?"
+puts "Enter a first name or quit to exit?"
 first_name = gets.chomp.downcase
 
+until first_name == "quit"
 #git the last name and convert it to the lower case letters
 puts "What is the last name?"
 last_name = gets.chomp.downcase
@@ -78,3 +79,7 @@ name = [new_first_name, new_last_name]
 name.reverse!
 result = name.join(" ")
 puts result
+
+puts "Enter a first name or quit to exit?"
+first_name = gets.chomp.downcase
+end
