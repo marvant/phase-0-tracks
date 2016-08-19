84ce0425b3a4fe6a2511359c6f69c937fc2a1454
marvan_t <marvan_t@hotmail.com>
2016-08-19 10:02:17 -0700

    give santa attributes for christmas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -9,11 +9,38 @@ class Santa
 		type
 	end
 
-	def initialize
+	def initialize (gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 end
 
-x = Santa.new
-x.speak
-x.eat_milk_and_cookies("oreo")
+# x = Santa.new
+# x.speak
+# x.eat_milk_and_cookies("oreo")
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+# p santas
+
+x = Santa.new("male", "black")
+p x
+p x.speak
+p x.eat_milk_and_cookies("oreo")
