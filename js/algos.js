0fec9ccf950972566b920cdab701905f0addcf92
marvan_t <marvan_t@hotmail.com>
2016-08-28 16:06:33 -0700

    add function to compare two objects and return true if any key value match and return false otherwise

--- js/algos.js
+++ js/algos.js
@@ -18,3 +18,38 @@ function LongestPhrase (array) {
 
 LongestPhrase(["long phrase", "longest phrase", "longer phrase"]);
 
+//create an object
+// function PersonalInfo(name, age){
+// 	this.name = name;
+// 	this.age = age;
+// }
+
+// obj1 = {name: "Steven", age: 54}
+// obj2 = {name: "Tamir", age: 55}
+// console.log(Object.keys(obj1).length);
+// var keys = Object.keys(obj1);
+// console.log(keys);
+// console.log(obj2[keys[1]]);
+
+
+// create a function that takes two objects
+function compare(obj1, obj2){
+	// get the keys for one of the objects
+	// only need the keys for one object. program will run faster if we use keys for the smaller object
+	// but for this program we just use the keys from the first object
+	var keys = Object.keys(obj1);
+	// define a result variable ans set its value to false. once a match is found it will be changed to true in if statement
+	var result = false
+	// for loop to go through each key and value pair using the keys
+	for (i=0; i < keys.length; i++){
+		// if statement to compare key value pair and return true if finds any match
+		if (obj1[keys[i]] == obj2[keys[i]]){
+			result = true;
+		} 
+	}
+	console.log(result);
+}
+compare({name: "Steven", age: 54}, {name: "Tamir", age: 54, eyecolor: "brown"});
+compare({make: "VW", model: "GTI", year: 2012}, {"make": "VW", model: "Jetta", year: 2005});
+compare({make: "VW", model: "GTI", year: 2012}, {"make": "Benz", model: "GLK", year: 2016});
+
